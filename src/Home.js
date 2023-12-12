1f9e05ee1d1c98e868a7af7c919bfb333b28a4a8
neeraj <neerajpc799@gmail.com>
2023-12-12 23:25:19 +0530

    The usestate is used.

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useState } from 'react'
+import BlogList from './BlogList'
+
+function Home() {
+  const [blogs, setblogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
+    
+  return (
+    <div className="home">
+      <BlogList/>
+        
+    </div>
+  )
+}
+
+export default Home
